fba0be6223132dde485758e6162a2b843ba0e1a8
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-07-26 14:45:33 +0200

    clone repo

--- public/js/app.js
+++ public/js/app.js
@@ -49611,9 +49611,9 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Daniele\Desktop\Boolean\laravel-one-to-many\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! C:\Users\Daniele\Desktop\Boolean\laravel-one-to-many\resources\sass\app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! C:\Users\Daniele\Desktop\Boolean\laravel-one-to-many\resources\sass\front.scss */"./resources/sass/front.scss");
+__webpack_require__(/*! C:\Users\Daniele\Desktop\Boolean\laravel-api\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! C:\Users\Daniele\Desktop\Boolean\laravel-api\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! C:\Users\Daniele\Desktop\Boolean\laravel-api\resources\sass\front.scss */"./resources/sass/front.scss");
 
 
 /***/ })
--- public/js/front.js
+++ public/js/front.js
@@ -12563,7 +12563,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Daniele\Desktop\Boolean\laravel-one-to-many\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Users\Daniele\Desktop\Boolean\laravel-api\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
